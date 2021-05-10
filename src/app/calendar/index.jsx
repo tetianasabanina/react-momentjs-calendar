dcd0fb28d7f751a62de7ee2cd5812debe53185cc
tatianasabanina <tatiana08st@gmail.com>
2021-05-10 09:42:00 +0300

    Fix script

--- src/app/calendar/index.jsx
+++ src/app/calendar/index.jsx
@@ -7,56 +7,10 @@ import "./styles.css";
 
 export default function Calendar({ value, onChange }) {
   const [calendar, setCalendar] = useState([]);
-  // const [value, setValue] = useState(moment());
 
   useEffect(() => {
     setCalendar(buildCalendar(value));
   }, [value]);
-
-  // function buildCalendar(date) {
-  //   const a = [];
-
-  //   const startDay = date.clone().startOf("month").startOf("week");
-  //   const endDay = date.clone().endOf("month").endOf("week");
-
-  //   const _date = startDay.clone().subtract(1, "day");
-
-  //   while (_date.isBefore(endDay, "day")) {
-  //     a.push(
-  //       Array(7)
-  //         .fill(0)
-  //         .map(() => _date.add(1, "day").clone())
-  //     );
-  //   }
-  //   return a;
-  // }
-
-  // function isSelected(day) {
-  //   return value.isSame(day, "day");
-  // }
-
-  // function beforeToday(day) {
-  //   return moment(day).isBefore(new Date(), "day");
-  // }
-
-  // function isToday(day) {
-  //   return moment(new Date()).isSame(day, "day");
-  // }
-
-  // function dayStyles(day) {
-  //   if (beforeToday(day)) return "before";
-  //   if (isSelected(day)) return "selected";
-  //   if (isToday(day)) return "today";
-  //   return "";
-  // }
-
-  // function currMonthName() {
-  //   return value.format("MMMM");
-  // }
-
-  // function currYear() {
-  //   return value.format("YYYY");
-  // }
 
   return (
     <div>
